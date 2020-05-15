84afe4590906d9aaff140c2469d1452be8a05d2c
alberaja <alberto.aja12@gmail.com>
2020-05-15 13:27:16 +0200

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\ruedadelasierra-bot-Web-Deploy.pubxml -p:Password=QmJsmkPou3LfqnZ5NdJnKgpT3K91B0w8x2RyPEslzir6JfAexfwmL5vvttpy
+
